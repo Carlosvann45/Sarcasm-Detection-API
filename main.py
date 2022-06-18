4f70e68dd2657304c21026468c09a205990d4e17
carlos <clsantiago@student.fullsail.edu>
2022-06-18 02:00:55 -0400

    modified endpoint to return json anwser and throw 503 if error is thrown

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import status as status
 from flask import *
 import pandas as pd
 import numpy as np
@@ -5,33 +6,40 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 
-
 app = Flask(__name__)
 
 
 @app.route('/containsSarcasm', methods=['POST'])
 def run_sarcasm_check():
-    json_data = request.get_json()
-    phrase = json_data['phrase']
+    try:
+        json_data = request.get_json()
+        phrase = json_data['phrase']
 
-    # Trains model on api load
-    data = pd.read_json("Sarcasm2.json", lines=True)
-    data["is_sarcastic"] = data["is_sarcastic"].map({0: "Not Sarcasm", 1: "Sarcasm"})
-    data = data[["headline", "is_sarcastic"]]
-    x = np.array(data["headline"])
-    y = np.array(data["is_sarcastic"])
+        # reads json from file nad sets it up for model
+        data = pd.read_json("Sarcasm.json", lines=True)
+        data["is_sarcastic"] = data["is_sarcastic"].map({0: "Not Sarcasm", 1: "Sarcasm"})
+        data = data[["headline", "is_sarcastic"]]
+        x = np.array(data["headline"])
+        y = np.array(data["is_sarcastic"])
 
-    cv = CountVectorizer()
-    x = cv.fit_transform(x)  # Fit the Data
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
-    model = BernoulliNB()
-    model.fit(x_train, y_train)
+        # Trains model on values based from Bernoulli's algorithm
+        cv = CountVectorizer()
+        x = cv.fit_transform(x)  # Fit the Data
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
+        model = BernoulliNB()
+        model.fit(x_train, y_train)
 
-    # checks trained model with phrase sent through request
-    data = cv.transform([phrase]).toarray()
-    output = model.predict(data)
+        # checks trained model with phrase sent through request
+        data = cv.transform([phrase]).toarray()
+        output = model.predict(data)
 
-    return output[0]
+        answer = {'answer': output[0]}
+
+    except BaseException as err:
+        print(err)
+        return Response('Server Unavailable', status=503)
+
+    return jsonify(answer)
 
 
 if __name__ == '__main__':
