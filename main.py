e424941cbc3c9a2a06b4a7994eb4306a1904150f
carlos <clsantiago@student.fullsail.edu>
2022-06-18 00:58:22 -0400

    fixed json error

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ app = Flask(__name__)
 def run_sarcasm_check():
     json_data = request.get_json()
     phrase = json_data['phrase']
-    
+
     # Trains model on api load
     data = pd.read_json("Sarcasm.json", lines=True)
+    data["is_sarcastic"] = data["is_sarcastic"].map({0: "Not Sarcasm", 1: "Sarcasm"})
     data = data[["headline", "is_sarcastic"]]
     x = np.array(data["headline"])
     y = np.array(data["is_sarcastic"])
