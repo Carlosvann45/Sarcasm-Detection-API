72eceb92356f86cdad4413ea100220716d69be22
carlos <clsantiago@student.fullsail.edu>
2022-06-17 21:03:58 -0400

    initial project commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import *
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+
+
+app = Flask(__name__)
+
+
+@app.route('/containsSarcasm', methods=['POST'])
+def run_sarcasm_check():
+    phrase = request.get_json()['phrase']
+
+    # Trains model on api load
+    data = pd.read_json("Sarcasm.json", lines=True)
+    data = data[["headline", "is_sarcastic"]]
+    x = np.array(data["headline"])
+    y = np.array(data["is_sarcastic"])
+
+    cv = CountVectorizer()
+    x = cv.fit_transform(x)  # Fit the Data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
+    model = BernoulliNB()
+    model.fit(x_train, y_train)
+
+    # checks trained model with phrase sent through request
+    data = cv.transform(phrase).toarray()
+    output = model.predict(data)
+
+    return output
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
